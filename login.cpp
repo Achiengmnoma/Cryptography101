b657d40a9916b19270f552701ac3f2cbfad15f50
DanielNiven98 <91530416+DanielNiven98@users.noreply.github.com>
2023-10-16 15:11:52 +0100

    hash_string intitial method added

--- login.cpp
+++ login.cpp
@@ -10,3 +10,14 @@ int main() {
   if (auth) authenticated("user");
   else rejected("user");
 }
+
+void sha256_hash_string(const unsigned char hash[SHA256_DIGEST_LENGTH], char outputBuffer[65]) {
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        std::sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+    outputBuffer[64] = 0; 
+
+    // Print the formatted hash
+    std::cout << "Formatted Hash: " << outputBuffer << std::endl;
+}
+
