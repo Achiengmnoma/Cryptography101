c60bc27cd2f1ee32a387f2328243d585de4fec2a
Adam47 <71328944+AmSmef@users.noreply.github.com>
2023-10-20 16:48:56 +0100

    Update Login.cpp
    simple username and password input, with hashing of the inputted password. Also reading from a file, and saving the contents within Stacey's set

--- login.cpp
+++ login.cpp
@@ -1,57 +1,96 @@
 #include <iostream>
-#include "authlib.h"
-#include <openssl/sha.h>
+#include <iomanip>>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <set>
+#include "authlib.h"
+#include <openssl/sha.h>
 
 using namespace std;
-string username;
-string passwords;
+
+string username_input() {
+
+	string username;
+	cout << "Please type your username: ";
+	cin >> username;
+	cout << "Your username is: " << username << endl;
+	return username;
+
+}
+
+string password_input() {
+
+	string password;
+	cout << "Type your password: ";
+	cin >> password;
+	cout << "Your password is: " << password << endl;
+	return password;
+
+}
+
+string sha256(const string str) {
+
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, str.c_str(), str.size());
+	SHA256_Final(hash, &sha256);
+	stringstream ss;
+
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+		ss << hex << setw(2) << setfill('0') << (int)hash[i];
+	}
+	return ss.str();
+
+}
+
+set<string>readPasswordtxt() {
+	//declares an empty set
+	set<string>saved_user_pass;
+	ifstream rFile("passwords.txt");
+
+	if (rFile.is_open()) {
+		string lines;
+		while (getline(rFile, lines)) {
+			saved_user_pass.insert(lines);
+		}
+		rFile.close();
+	}
+	else {
+		cout << "Unable to open and Read from File";
+	}
+	
+	return saved_user_pass;
+}
+
+void displaySet(const set<string>& strSet) {
+
+	for (const string& str : strSet) {
+		cout << str << ", ";
+	}
+
+	cout << endl;
+
+}
+
 int main() {
-  bool auth = true;
- 
-  if (auth) authenticated("user");
-  else rejected("user");
-}
+	bool auth = true;
 
-void sha256_hash_string(const unsigned char hash[SHA256_DIGEST_LENGTH], char outputBuffer[65]) {
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        std::sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
-    }
-    outputBuffer[64] = 0; 
+	if (auth) authenticated("user");
+	else rejected("user");
 
-    // Print the formatted hash
-    std::cout << "Formatted Hash: " << outputBuffer << std::endl;
-}
+	string username = username_input();
+	string password = password_input();
+	
+	cout << sha256(password) << endl << endl;
+	
+	set<string> passwordSet = readPasswordtxt();
 
-//reads and stores passwords in the set
-set<string>readPasswordtxt(){
-      //declares an empty set
-      set<string>saved_user_pass;
-      ifstream rFile("passwords.txt");
+	if (!passwordSet.empty()) {
+		cout << "Contents of the set:" << endl;
+		displaySet(passwordSet);
+	}
 
-      if(rFile.is_open()){
-      string lines;
-      while(getline(rFile,lines)){
-              saved_user_pass.insert(lines);
-      }
-      rFile.close();
-      }else{
-      cout << "Unable to open and Read from File";
-      }return saved_user_pass;
-}
-
-
-//checks whether password exist in the set of stored passwords
-void checkLogins(set<string>readPasswordtxt,string passwords){
-    string loginInfo = username + ":" + passwords;
-    set<string>::iterator itn = find(readPasswordtxt.begin(),readPasswordtxt.end(),loginInfo);
-
-    if(itn != readPasswordtxt.end()){
-      authenticated(username);
-      exit(0);
-    }else{
-      rejected(username);
-    }
+	return 0;
 }
