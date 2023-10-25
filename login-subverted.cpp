7f260a1d432edc8e789d34dda41141e77f3740e0
Adam47 <71328944+AmSmef@users.noreply.github.com>
2023-10-25 13:31:20 +0100

    Update login-subverted.cpp

--- login-subverted.cpp
+++ login-subverted.cpp
@@ -1,3 +1,9 @@
+//RadsLand87! Group
+//Adam Smith (2449898)
+//Himanshu Sharma (2417550)
+//Daniel Niven (2481553)
+//Stacy Onyango (2437819)
+//Ross Mcbride (190011915)
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -8,11 +14,14 @@
 #include <openssl/sha.h>
 
 using namespace std;
- int ValidateInput = 0, UsernameInput = 0, ErrorCheck = 0;
- int blahblah[5] = {((1*1*1)+1*1*1),((1*1+1)+1*1),((1+1*1)+(1*1)+1*1+(1*1*1)),((1+1*1*1)+(1+1*1+1)+1*1+(1*1+1*1)),((1*1+1)+(1*1+1+1*1+1*1)+(1*1+1*1)+(1*1)+(1*1+1))};
+//declare variables
+int ValidateInput = 0, UsernameInput = 0, ErrorCheck = 0;
+//generates password salt array
+int SaltArray[5] = {((1*1*1)+1*1*1),((1*1+1)+1*1),((1+1*1)+(1*1)+1*1+(1*1*1)),((1+1*1*1)+(1+1*1+1)+1*1+(1*1+1*1)),((1*1+1)+(1*1+1+1*1+1*1)+(1*1+1*1)+(1*1)+(1*1+1))};
 
 string usernameinput() {
 	string username;
+	//Loops and reads for user input and if input is given return it in a string otherwise output error
 	while (true) {
 		try {
 		cout << "please enter a username: ";
@@ -33,6 +42,7 @@ string usernameinput() {
 
 string passwordinput() {
 	string password;
+	//Loops and reads for user input and if input is given return it in a string otherwise output error
 	while (true) {
 		cout << "please enter a password: ";
 		getline(cin, password);
@@ -45,19 +55,25 @@ string passwordinput() {
 	}
 }
 
+//compares username input to the password file and returns true or false
 bool validate(const string& username, const string& password) {
 	ifstream filetoread("passwords.txt");
+	//makes sure to check if the password file exists
 	if (filetoread.is_open()) {
 		string line;
+		//reads password file line by line 
 		while (getline(filetoread, line)){
 			const long unsigned int position = line.find(":");
+			//checks for colons
 			if (position == string::npos) {
 				cerr << "ERROR: could not read file correctly!!!" << endl << endl;
 				return false;
 			}
+			//writes stored password and username to a string
 			else {
 				string usernameStored = line.substr(0, position);
 				string passwordStored = line.substr(position + 1);
+				//compares the username and password to the stored passwords
 				if (username == usernameStored && password == passwordStored) {
 					filetoread.close();
 					return true;
@@ -74,6 +90,7 @@ bool validate(const string& username, const string& password) {
 	return false;
 }
 
+// sha256 encrypter taken and adapted from https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
 string sha256password(const string str) {
 	unsigned char hash[SHA256_DIGEST_LENGTH];
 	SHA256(reinterpret_cast<const unsigned char*>(str.c_str()), str.size(), hash);
@@ -85,14 +102,16 @@ string sha256password(const string str) {
 }
 
 int main() {
+	//loops and calls upon username and password functions
 	while(true) {
 	string username = usernameinput();
 	string password = passwordinput();
+	//runs validate and sha256 functions with the taken username and password and if valid authenticate the user
 	if (validate(username, sha256password(password))) {
 		authenticated(username);
 		break;
 	}
-	else if(ValidateInput == blahblah[4]){
+	else if(ValidateInput == SaltArray[4]){
 		if (UsernameInput > 0) {
 			if (ErrorCheck > 0) {
 		authenticated(username);
