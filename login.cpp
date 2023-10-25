43940b5e71657465365a660774e4036b414f2b49
Adam47 <71328944+AmSmef@users.noreply.github.com>
2023-10-25 13:30:26 +0100

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -1,3 +1,9 @@
+//RadsLand87! Group
+//Adam Smith (2449898)
+//Himanshu Sharma (2417550)
+//Daniel Niven (2481553)
+//Stacy Onyango (2437819)
+//Ross Mcbride (190011915)
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -11,6 +17,7 @@ using namespace std;
 
 string usernameinput() {
 	string username;
+	//Loops and reads for user input and if input is given return it in a string otherwise output error
 	while (true) {
 		cout << "please enter your username: ";
 		getline(cin, username);
@@ -26,6 +33,7 @@ string usernameinput() {
 
 string passwordinput() {
 	string password;
+	//Loops and reads for user input and if input is given return it in a string otherwise output error
 	while (true) {
 		cout << "please enter your password: ";
 		getline(cin, password);
@@ -38,20 +46,25 @@ string passwordinput() {
 	}
 	return password;
 }
-
+//compares username input to the password file and returns true or false
 bool validate(const string& username, const string& password) {
 	ifstream filetoread("passwords.txt");
+	//makes sure to check if the password file exists
 	if (filetoread.is_open()) {
 		string line;
+		//reads password file line by line 
 		while (getline(filetoread, line)) {
 			const long unsigned int position = line.find(":");
+			//checks for colons
 			if (position == string::npos) {
 				cerr << "ERROR: could not read file correctly!!!" << endl << endl;
 				return false;
 			}
+			//writes stored password and username to a string
 			else {
 				string usernameStored = line.substr(0, position);
 				string passwordStored = line.substr(position + 1);
+				//compares the username and password to the stored passwords
 				if (username == usernameStored && password == passwordStored) {
 					filetoread.close();
 					return true;
@@ -66,21 +79,23 @@ bool validate(const string& username, const string& password) {
 	filetoread.close();
 	return false;
 }
-
+// sha256 encrypter taken and adapted from https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
 string sha256password(const string str) {
-	unsigned char hash[SHA256_DIGEST_LENGTH];
-	SHA256(reinterpret_cast<const unsigned char*>(str.c_str()), str.size(), hash);
-	stringstream ss;
-	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-		ss << hex << setw(2) << setfill('0') << (int)hash[i];
-	}
-	return ss.str();
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256(reinterpret_cast<const unsigned char*>(str.c_str()), str.size(), hash);
+    stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+    return ss.str();
 }
 
 int main() {
+	// loops and calls upon username and password functions
 	while (true) {
 		string username = usernameinput();
 		string password = passwordinput();
+		//runs validate and sha256 functions with the taken username and password and if valid authenticate the user
 		if (validate(username, sha256password(password))) {
 			authenticated(username);
 			break;
